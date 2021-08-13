89e4730a8f80ec351e5171abd8d5e42fbbb09e2b
Ian Dennis Miller <ian@iandennismiller.com>
2021-08-13 15:09:08 -0400

    include accuracy stuff

---
+++ bin/accuracy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+function run_one {
+    PMSP_RANDOM_SEED=$1 \
+        PMSP_DILUTION=$2 \
+        PMSP_PARTITION=$3 \
+        ./alens.sh \
+        $4 &
+}
+
+function run_one_probes {
+    run_one $1 $2 $3 ./accuracy-probes-dt-100.tcl
+}
+
+function run_one_partition_probes {
+    PARITION=$1
+    run_one_probes 1 1 $PARITION
+    run_one_probes 1 2 $PARITION
+    run_one_probes 1 3 $PARITION
+}
+
+run_one_partition_probes 0
+run_one_partition_probes 1
+run_one_partition_probes 2
