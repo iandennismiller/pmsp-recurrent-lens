a3d8c51f868266a24376c47a6c0413d3f0f5aeeb
Ian Dennis Miller <ian@iandennismiller.com>
2021-08-15 22:47:33 +0000

    also do accuracy of anchors and base vocab

--- bin/accuracy.sh
+++ bin/accuracy.sh
@@ -19,6 +19,21 @@ function run_one_partition_probes {
     run_one_probes 1 3 $PARITION
 }
 
-run_one_partition_probes 0
-run_one_partition_probes 1
-run_one_partition_probes 2
+function run_one_pmsp_anchors {
+    run_one $1 $2 $3 ./src/run-accuracy-pmsp-anchors.tcl
+}
+
+function run_one_partition_pmsp_anchors {
+    PARITION=$1
+    run_one_probes 1 1 $PARITION
+    run_one_probes 1 2 $PARITION
+    run_one_probes 1 3 $PARITION
+}
+
+# run_one_partition_probes 0
+# run_one_partition_probes 1
+# run_one_partition_probes 2
+
+run_one_partition_pmsp_anchors 0
+run_one_partition_pmsp_anchors 1
+run_one_partition_pmsp_anchors 2
---
+++ src/run-accuracy-pmsp-anchors.tcl
@@ -0,0 +1,87 @@
+# pmsp-lens
+# Ian Dennis Miller
+# 2020-11-07
+
+source ./accuracy.tcl
+
+set dt 100
+set start_epoch 0
+set end_epoch 3999
+
+set random_seed $::env(PMSP_RANDOM_SEED)
+puts "Random seed: $random_seed"
+set dilution_amount $::env(PMSP_DILUTION)
+puts "Dilution amount: $dilution_amount"
+set partition_id $::env(PMSP_PARTITION)
+puts "Partition ID: $partition_id"
+
+# reproducible
+seed $random_seed
+
+# unique name of this script, for naming saved weights
+set script_name "cogsci-recurrent-dt-100-dilution-$dilution_amount-seed-$random_seed-partition-$partition_id-straight-through"
+
+# root of project is relative to this .tcl file
+set root_path "../"
+
+set examples_path "${root_path}/examples"
+# we always use an example file with dilution=3 to get all possible ancors
+set example_file "${examples_path}/cogsci-pmsp-added-partition-0-dilution-3.ex"
+
+set weights_path "${root_path}/var/${script_name}/weights"
+set results_path "${root_path}/var/${script_name}/results"
+
+set loggingInterval 1
+
+set log_accuracy_filename "${results_path}/accuracy-pmsp-anchors.tsv"
+file delete $log_accuracy_filename
+
+source ./network.tcl
+
+###
+# Settings
+
+setObj learningRate 0.05
+setObj momentum 0.98
+
+# p. 28 "the slight tendency for weights to decay towards zero was removed, to prevent the very small weight changes induced by low-frequency words - due to their very small scaling factors - from being overcome by the tendency of weights to shrink towards zero."
+setObj weightDecay 0.00000
+
+# "output units are trained to targets of 0.1 and 0.9"
+setObj targetRadius 0.1
+
+loadExamples $example_file -s vocab
+exampleSetMode vocab PERMUTED
+useTrainingSet vocab
+
+setObj vocab.minTime 2.0
+setObj vocab.maxTime 2.0
+setObj vocab.graceTime 1.0
+
+# install hook to log accuracy
+setObj postExampleProc { logAccuracyHook }
+
+# Need to view units to be able to access the history arrays.
+# TODO: ensure it updates per example, not per batch
+# (updates 3: update after each example)
+# viewUnits -updates 3
+# viewUnits
+# could set target history property?
+# consider testing the "-numexamples 2" and manually run through a couple
+
+for { set epoch $start_epoch } { $epoch <= $end_epoch } { incr epoch 1 } {
+    puts "value of epoch: $epoch"
+
+    # load a network that has been already trained
+    resetNet
+    loadWeights "${weights_path}/${epoch}.wt.gz"
+
+    # `test` doesn't provide access to hidden units via postExampleProc
+    # use train instead
+    test
+    # train 1
+}
+
+saveAccuracyResults $log_accuracy_filename
+
+exit
