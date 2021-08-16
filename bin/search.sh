f2dd4a3f51f6886bd558b50445ffa11c09654fa6
Ian Dennis Miller <ian@iandennismiller.com>
2021-08-16 16:02:56 +0000

    refactor to parallelize the frequency

--- bin/search.sh
+++ bin/search.sh
@@ -4,21 +4,32 @@ function run_alens {
     PMSP_RANDOM_SEED=$1 \
         PMSP_DILUTION=$2 \
         PMSP_PARTITION=$3 \
-        ./bin/alens.sh \
-        $4 &
+        PMSP_FREQ=$4
+        ./bin/alens.sh -batch \
+        $5 &
 }
 
 function search_one_dilution {
-    run_alens $1 $2 $3 ./src/search-frequency.tcl
+    run_alens $1 $2 $3 $4 ./src/search-frequency.tcl
 }
 
 function search_one_partition {
     PARITION=$1
-    search_one_dilution 1 1 $PARITION
-    search_one_dilution 1 2 $PARITION
-    search_one_dilution 1 3 $PARITION
+    FREQUENCY=$2
+    search_one_dilution 1 1 $PARITION $FREQUENCY
+    search_one_dilution 1 2 $PARITION $FREQUENCY
+    search_one_dilution 1 3 $PARITION $FREQUENCY
 }
 
-search_one_partition 0
-search_one_partition 1
-search_one_partition 2
+function search_one_frequency {
+    FREQUENCY=$1
+    search_one_partition 0 $FREQUENCY
+    search_one_partition 1 $FREQUENCY
+    search_one_partition 2 $FREQUENCY
+}
+
+find . -name "search-frequency.tsv" -delete
+
+for FREQUENCY in 0.00001 0.0001 0.001 0.01 0.1 1 10 100 1000 10000 100000; do
+    search_one_frequency $FREQUENCY
+done
--- src/search-frequency.tcl
+++ src/search-frequency.tcl
@@ -7,6 +7,8 @@ set dilution_amount $::env(PMSP_DILUTION)
 puts "Dilution amount: $dilution_amount"
 set partition_id $::env(PMSP_PARTITION)
 puts "Partition ID: $partition_id"
+set freq $::env(PMSP_FREQ)
+puts "Frequency: $freq"
 
 # reproducible
 seed $random_seed
@@ -20,7 +22,7 @@ set results_path "${root_path}/var/${script_name}/results"
 set examples_path "${root_path}/examples"
 
 set output_file "$results_path/search-frequency.tsv"
-file delete $output_file
+# file delete $output_file
 
 source ./network.tcl
 source ./parameters.tcl
@@ -28,7 +30,7 @@ source ./parameters.tcl
 ###
 # Sweep frequency parameter space
 
-set freqs [ list 0.00001 0.0001 0.001 0.01 0.1 1 10 100 1000 10000 100000 ]
+# set freqs [ list 0.00001 0.0001 0.001 0.01 0.1 1 10 100 1000 10000 100000 ]
 
 set example_test_file "${examples_path}/pmsp/pmsp-train-the-normalized.ex"
 loadExamples $example_test_file -s "vocab_pmsp"
@@ -38,34 +40,29 @@ useTestingSet "vocab_pmsp"
 # test.group.crit 0.5, will test at group level, will pass criterion if all units on corrct side of 0.5
 # setObj test.group.crit 0.5
 
-foreach {freq} $freqs {
-    puts "frequency: $freq"
+set example_file "${root_path}/examples/frequency-search/freq-$freq-partition-$partition_id-dilution-$dilution_amount.ex"
+loadExamples $example_file -s "vocab_anchors"
+exampleSetMode "vocab_anchors" PERMUTED
+useTrainingSet "vocab_anchors"
 
-    set example_file "${root_path}/examples/frequency-search/freq-$freq-partition-$partition_id-dilution-$dilution_amount.ex"
-    loadExamples $example_file -s "vocab_anchors"
-    exampleSetMode "vocab_anchors" PERMUTED
-    useTrainingSet "vocab_anchors"
+setObj vocab_anchors.minTime 2.0
+setObj vocab_anchors.maxTime 2.0
+setObj vocab_anchors.graceTime 1.0
 
-    setObj vocab_anchors.minTime 2.0
-    setObj vocab_anchors.maxTime 2.0
-    setObj vocab_anchors.graceTime 1.0
+# resume training at the end of the PMSP, before anchor introduction
+loadWeights "${weights_path}/1999.wt.gz"
 
-    # resume training at the end of the PMSP, before anchor introduction
-    loadWeights "${weights_path}/1999.wt.gz"
+for { set epoch_idx 1999} {$epoch_idx <= 2300} {incr epoch_idx} {
+    train 1
 
-    for { set epoch_idx 1999} {$epoch_idx <= 2300} {incr epoch_idx} {
-        train 1
+    # at each epoch, run test on base vocabulary corpus, -return arg to test command? instead of printing, it returns an object; test_result.percent_corret or test_result.error; will be the percent correct of base vocabulary; just keep minimum results across this sweep, save minimum and key it according to dilution level and scaling frequency for language
+    test
 
-        # at each epoch, run test on base vocabulary corpus, -return arg to test command? instead of printing, it returns an object; test_result.percent_corret or test_result.error; will be the percent correct of base vocabulary; just keep minimum results across this sweep, save minimum and key it according to dilution level and scaling frequency for language
-        test
+    puts $Test(percentCorrect)
 
-        puts $Test(percentCorrect)
-
-        set f [open $output_file "a"]
-        puts $f "$freq\t$epoch_idx\t$Test(percentCorrect)"
-        close $f
-
-    }
+    set f [open $output_file "a"]
+    puts $f "$freq\t$epoch_idx\t$Test(percentCorrect)"
+    close $f
 
 }
 
