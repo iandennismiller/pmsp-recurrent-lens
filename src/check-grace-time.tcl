935bf604e001112cc5b198c7cf0069e0e2af8f94
Ian Dennis Miller <ian@iandennismiller.com>
2021-08-22 19:56:49 +0000

    notes from meeting

--- src/check-grace-time.tcl
+++ src/check-grace-time.tcl
@@ -25,21 +25,25 @@ source ./parameters.tcl
 ###
 # Sweep frequency parameter space
 
-set example_test_file "${examples_path}/pmsp/pmsp-train-the-normalized.ex"
-loadExamples $example_test_file -s "vocab_pmsp"
-exampleSetMode "vocab_pmsp" PERMUTED
-useTestingSet "vocab_pmsp"
+# set example_test_file "${examples_path}/pmsp/pmsp-train-the-normalized.ex"
+# loadExamples $example_test_file -s "vocab_pmsp"
+# exampleSetMode "vocab_pmsp" PERMUTED
+# useTestingSet "vocab_pmsp"
+
 
 set example_file "${examples_path}/cogsci/cogsci-pmsp-added-partition-$partition_id-dilution-${dilution_amount}.ex"
 loadExamples $example_file -s "vocab_anchors"
 exampleSetMode "vocab_anchors" PERMUTED
 useTrainingSet "vocab_anchors"
 
+setObj testGroupCrit 0.5
+setObj trainGroupCrit 0.5
+
 setObj vocab_anchors.minTime 2.0
 setObj vocab_anchors.maxTime 2.0
 # when vocab_anchors.graceTime is 1.991 there's 0 error
 
-foreach {grace_time_iter} [ list 1.899 1.990 1.991 ] {
+foreach {grace_time_iter} [ list 1.0 1.899 1.990 1.991 ] {
     puts "setObj vocab_anchors.graceTime $grace_time_iter"
     setObj vocab_anchors.graceTime $grace_time_iter
     loadWeights "${weights_path}/1999.wt.gz"
